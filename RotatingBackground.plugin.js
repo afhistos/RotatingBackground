cfeca3a9badd6c62395e2a6d68855bf263e7388a
afhistos <32820096+afhistos@users.noreply.github.com>
2022-02-01 19:14:20 +0100

    Delete RotatingBackground.plugin.js

--- RotatingBackground.plugin.js
+++
@@ -1,8 +0,0 @@
-/**
-    * @name RotatingBackground
-    * @source https://github.com/QWERTxD/BetterDiscordPlugins/blob/main/CallTimeCounter/CallTimeCounter.plugin.js
-    * @description Make the background rotate
-    * @updateUrl https://raw.githubusercontent.com/QWERTxD/BetterDiscordPlugins/main/CallTimeCounter/CallTimeCounter.plugin.js
-    * @website https://github.com/afhistos/RotatingBackground
-    */
-    
