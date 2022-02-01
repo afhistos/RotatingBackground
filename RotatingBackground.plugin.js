f5740260cf6d0bc7b92aeb7dc917bef7c6d5ce2d
afhistos <32820096+afhistos@users.noreply.github.com>
2022-02-01 19:13:51 +0100

    Add files via upload

---
+++ RotatingBackground.plugin.js
@@ -0,0 +1,8 @@
+/**
+    * @name RotatingBackground
+    * @source https://github.com/QWERTxD/BetterDiscordPlugins/blob/main/CallTimeCounter/CallTimeCounter.plugin.js
+    * @description Make the background rotate
+    * @updateUrl https://raw.githubusercontent.com/QWERTxD/BetterDiscordPlugins/main/CallTimeCounter/CallTimeCounter.plugin.js
+    * @website https://github.com/afhistos/RotatingBackground
+    */
+    
