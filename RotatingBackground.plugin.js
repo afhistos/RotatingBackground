97f98480ea29615e8a302ec4eed99c6a5ca11f7b
afhistos <32820096+afhistos@users.noreply.github.com>
2022-02-01 19:14:30 +0100

    Add files via upload

---
+++ RotatingBackground.plugin.js
@@ -0,0 +1,8 @@
+/**
+    * @name RotatingBackground
+    * @source 
+    * @description Make the background rotate
+    * @updateUrl 
+    * @website https://github.com/afhistos/RotatingBackground
+    */
+    
